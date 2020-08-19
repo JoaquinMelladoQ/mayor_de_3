9035f45fe35617d3268aca9ee7e53dee76b0bdfa
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-08-19 16:36:27 -0400

    initial commit

---
+++ mayor_de_3.rb
@@ -0,0 +1,18 @@
+#Datos del usuario
+primer_dato_obtenido = ARGV[0].to_i
+segundo_dato_obtenido = ARGV[1].to_i
+tercer_dato_obtenido = ARGV[2].to_i
+
+#situaciones de datos
+
+#primer dato obtenido
+if (primer_dato_obtenido > segundo_dato_obtenido && primer_dato_obtenido > tercer_dato_obtenido)
+    puts "#{primer_dato_obtenido} es mayor"
+#segundo dato obtenido
+elsif (segundo_dato_obtenido > tercer_dato_obtenido && segundo_dato_obtenido > primer_dato_obtenido)
+    puts "#{segundo_dato_obtenido} es mayor"
+#tercer dato obtenido
+else (tercer_dato_obtenido > primer_dato_obtenido && tercer_dato_obtenido > segundo_dato_obtenido)
+    puts "#{tercer_dato_obtenido} es mayor"
+end
+

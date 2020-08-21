c1c29a5897f0196707db7496946e3d2f016c5773
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-08-21 19:13:45 -0400

    removing text

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -7,12 +7,12 @@ tercer_dato_obtenido = ARGV[2].to_i
 
 #primer dato obtenido
 if (primer_dato_obtenido > segundo_dato_obtenido && primer_dato_obtenido > tercer_dato_obtenido)
-    puts "#{primer_dato_obtenido} es mayor"
+    puts "#{primer_dato_obtenido}"
 #segundo dato obtenido
 elsif (segundo_dato_obtenido > tercer_dato_obtenido && segundo_dato_obtenido > primer_dato_obtenido)
-    puts "#{segundo_dato_obtenido} es mayor"
+    puts "#{segundo_dato_obtenido}"
 #tercer dato obtenido
 else (tercer_dato_obtenido > primer_dato_obtenido && tercer_dato_obtenido > segundo_dato_obtenido)
-    puts "#{tercer_dato_obtenido} es mayor"
+    puts "#{tercer_dato_obtenido}"
 end
 
